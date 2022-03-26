aaa0152bd3fd6fad56eca353063581caec71c313
Adriana <am20dipi@alum.siena.edu>
2022-03-26 16:52:55 -0400

    Exploring the use of layouts and partials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,40 @@ class UsersController < ApplicationController
     # show
 
     # index
-
+    def index
+        @users = User.all
+    end
     
+
+
+    # about
+    def about
+
+    end
+
+    def create
+        @user = User.new(params[:user])
+        if @user.save
+          flash[:success] = "User successfully created"
+          redirect_to @user
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update_attributes(params[:user])
+          flash[:success] = "User was successfully updated"
+          redirect_to @user
+        else
+          flash[:error] = "Something went wrong"
+          render 'edit'
+        end
+    end
+    
+    
+
+
 end
